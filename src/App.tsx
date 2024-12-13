d6d9fc2f1673dceba184cc0a580f79c086098394
JhonRobert20 <jhonbara51214@gmail.com>
2024-12-13 11:23:28 +0100

    feat: intial skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,40 @@
-import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import React, { useState } from 'react';
+import { SongDetails } from '@/components/SongDetails';
+import { MusicPlayer } from '@/components/MusicPlayer';
 
-const App: React.FC = () => {
-  return <div>Hola mundo</div>;
-};
-export default App;
+const queryClient = new QueryClient();
+
+export default function App() {
+  const [selectedTrack, setselectedTrack] = useState<string | null>(null);
+  const [isSongDetailsOpen, setIsSongDetailsOpen] = useState<boolean>(false);
+  const [isPlayerOpen, setIsPlayerOpen] = useState<boolean>(false);
+
+  const closeSongDetails = () => {
+    setIsSongDetailsOpen(false);
+  };
+
+  const closePlayer = () => {
+    setIsSongDetailsOpen(false);
+    setIsPlayerOpen(false);
+    setselectedTrack(null);
+  };
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div className="music-app">
+        <div
+          className={`main-content ${selectedTrack && isSongDetailsOpen ? 'with-sidebar' : ''}`}
+        ></div>
+
+        {selectedTrack && isSongDetailsOpen && (
+          <SongDetails songId={selectedTrack} onClose={closeSongDetails} />
+        )}
+
+        {isPlayerOpen && (
+          <MusicPlayer trackId={selectedTrack} onClose={closePlayer} />
+        )}
+      </div>
+    </QueryClientProvider>
+  );
+}
