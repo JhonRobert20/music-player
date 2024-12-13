8ecba659b0fc2242019c4664c0d40e3eb84a5fd6
JhonRobert20 <jhonbara51214@gmail.com>
2024-12-13 11:57:54 +0100

    feat: logic in services

---
+++ src/services/track.ts
@@ -0,0 +1,11 @@
+import { QueryFunctionContext } from 'react-query';
+import {fetchFromApi} from "@/services/fetchFromApi";
+import { transformTrack } from '@/dtos/tracks';
+export const fetchTrackInfo = async ({
+  queryKey,
+}: QueryFunctionContext<[string, string]>) => {
+  const [, trackId] = queryKey;
+  const endpointTrack = `track/${trackId}`;
+  const apiResponse = await fetchFromApi(endpointTrack);
+  return transformTrack(apiResponse);
+};
