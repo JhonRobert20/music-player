17045fb7ebbb41eecb5065f29133d099659c51af
JhonRobert20 <jhonbara51214@gmail.com>
2024-12-15 21:21:01 +0100

    feat: music player modal

--- src/components/MusicPlayer.tsx
+++ src/components/MusicPlayer.tsx
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
 import ReactPlayer from 'react-player';
 import { useTrackInfo } from '@/hooks/useTrackInfo';
-export const MusicPlayer = ({
-  trackId,
-  onClose,
-}: {
-  trackId: string;
-  onClose: React.MouseEventHandler<HTMLButtonElement>;
-}) => {
-  const { data: trackInfo, isLoading, error } = useTrackInfo(trackId);
+import { useMusicContext } from '@/contexts/MusicContext';
+import { useSelectedSongContext } from '@/contexts/SelectedSongContext';
+import styles from '@/components/MusicPlayer.module.css';
+
+export const MusicPlayer = () => {
+  const { closeAll, isPlayerOpen, setIsPlayerOpen } = useMusicContext();
+  const { selectedSong, selectedSongIsNew, setSelectedSongIsNew } =
+    useSelectedSongContext();
+  const [closing, setClosing] = useState(false);
+  useEffect(() => {
+    if (selectedSongIsNew && selectedSong) {
+      setIsPlayerOpen(true);
+      setSelectedSongIsNew(false);
+    }
+  }, []);
+
+  if (!selectedSong || !isPlayerOpen) {
+    return null;
+  }
+
+  const { data: trackInfo, isLoading, error } = useTrackInfo(selectedSong);
 
   if (isLoading) return <div>Cargando...</div>;
   if (error instanceof Error)
@@ -17,13 +31,33 @@ export const MusicPlayer = ({
   if (!trackInfo)
     return <div>No se pudo obtener la información de la canción.</div>;
 
-  return (
-    <div>
-      <h2>{trackInfo.title}</h2>
-      <p>Artista: {trackInfo.artist.name}</p>
-      <p>Álbum: {trackInfo.album.title}</p>
-      <ReactPlayer url={trackInfo.preview} controls={true} playing={false} />
-      <button onClick={onClose} />
+  const handleClose = () => {
+    setClosing(true);
+    setTimeout(() => {
+      closeAll();
+      setClosing(false);
+    }, 400);
+  };
+
+  const modalContent = (
+    <div className={styles.overlay} onClick={handleClose}>
+      <div
+        className={`${styles.modal} ${closing ? styles.closing : ''}`}
+        onClick={e => e.stopPropagation()}
+      >
+        <button className={styles.closeButton} onClick={handleClose}>
+          ✖
+        </button>
+        <h2>{trackInfo.title}</h2>
+        <p>Artista: {trackInfo.artist.name}</p>
+        <p>Álbum: {trackInfo.album.title}</p>
+        <ReactPlayer url={trackInfo.preview} controls={true} playing={false} />
+      </div>
     </div>
+  );
+
+  return ReactDOM.createPortal(
+    modalContent,
+    document.getElementById('modal-root') as HTMLElement,
   );
 };
