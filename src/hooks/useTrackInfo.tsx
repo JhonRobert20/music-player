614026a5adcb82205e12f8cc94333b9a3c2eafab
JhonRobert20 <jhonbara51214@gmail.com>
2024-12-17 10:41:33 +0100

    refactor: to solve some bad performance

--- src/hooks/useTrackInfo.tsx
+++ src/hooks/useTrackInfo.tsx
@@ -2,7 +2,21 @@ import { useQuery } from 'react-query';
 import { fetchTrackInfo } from '@/services/track';
 
 export const useTrackInfo = (trackId: number | null) => {
-  return useQuery(['track', trackId], fetchTrackInfo, {
-    enabled: !!trackId,
-  });
+  const { data, isLoading, isError, error } = useQuery(
+    ['track', trackId],
+    fetchTrackInfo,
+    {
+      enabled: !!trackId,
+      staleTime: 5 * 60 * 1000,
+      cacheTime: 10 * 60 * 1000,
+      refetchOnWindowFocus: false,
+    },
+  );
+
+  return {
+    trackInfo: data,
+    isLoading,
+    isError,
+    error,
+  };
 };
