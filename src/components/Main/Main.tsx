d965181fa5a88a838578951067936106adf32968
JhonRobert20 <jhonbara51214@gmail.com>
2024-12-17 10:33:52 +0100

    refactor: open and close songDetails

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
-import { SongDetails } from '@/components/SongDetails';
+import { SongDetails } from '@/components/SongDetails/SongDetails';
 import { SearchResult } from '@/components/SearchResults/SearchResults';
 import * as styles from './Main.module.css';
 import { useMusicContext } from '@/contexts/MusicContext';
+import { MusicPlayer } from '@/components/MusicPlayer/MusicPlayer';
 
 export const Main = () => {
-  const { isSongDetailsOpen } = useMusicContext();
-  const gridClass = isSongDetailsOpen
-    ? styles.gridWithRightSidebar
-    : styles.gridWithoutRightSidebar;
+  const { isSongDetailsOpen, selectedSong } = useMusicContext();
+
+  const gridClass =
+    isSongDetailsOpen && selectedSong
+      ? styles.gridWithRightSidebar
+      : styles.gridWithoutRightSidebar;
 
   return (
     <div className={gridClass}>
       <SearchResult />
-      <SongDetails />
+      {isSongDetailsOpen && <SongDetails />}
+      <MusicPlayer />
     </div>
   );
 };
